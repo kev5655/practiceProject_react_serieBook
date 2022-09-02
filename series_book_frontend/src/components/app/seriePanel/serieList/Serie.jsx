6dca5337bda86481423312545d371d8919800638
kevin <kevin.zahn@bluewin.ch>
2022-09-02 15:58:52 +0200

    edit styles of Serie.jsx

--- series_book_frontend/src/components/app/seriePanel/serieList/Serie.jsx
+++ series_book_frontend/src/components/app/seriePanel/serieList/Serie.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import classes from './Serie.module.css'
 
@@ -6,64 +6,37 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import StarHalfRoundedIcon from '@mui/icons-material/StarHalfRounded';
 import StarOutlineRoundedIcon from '@mui/icons-material/StarOutlineRounded';
-import {fireChangeForInputTimeIfValid} from "@testing-library/user-event/dist/keyboard/shared";
 
 
 const Serie = (props) => {
 
-    //const [starsElements, setStarsElement] = useState(createStars());
-
-    //const createStars = () => {
-    //    let elements = []
-    //    for (let i = 0; i < 5; i++) {
-    //        if (props.stars > i) {
-    //            elements.push(<StarRoundedIcon className={classes.star} key={i}/>)
-    //        } else {
-    //            elements.push(<StarOutlineRoundedIcon className={classes.star} key={i}/>)
-    //        }
-    //    }
-    //    return elements
-    //}
-    //const addSerieHandler = (newSerie) => {
-//
-    //    setSeries((prevSerie) => {
-    //        console.log(newSerie)
-    //        console.log(prevSerie);
-    //        return [newSerie, ...prevSerie];
-    //    });
-    //    setState(ActivePanel.SerieList);
-    //}
-
-
     return (
-        <article className={classes.serie_context}>
-            <div className={classes.serie}>
-                <div>
-                    <div className={classes.serie_leftContent}>
-                        <title>{props.title}</title>
-                        <p>{props.session}S</p>
-                        <p>{props.episode}E</p>
-                    </div>
-
-                    <div>
-                        {props.stars.map((value, i) => {
-                            if (value === 1) {
-                                return (<StarRoundedIcon className={classes.star} key={i}/>)
-                            } else {
-                                return (<StarOutlineRoundedIcon className={classes.star} key={i}/>)
-                            }
-                        })}
-                    </div>
+        <article className={classes.serie}>
+            <div>
+                <div className={classes.serie_firstRow}>
+                    <title>{props.title}</title>
+                    <p>{props.session}S</p>
+                    <p>{props.episode}E</p>
                 </div>
 
-                <MoreHorizIcon
-                    onClick={props.editSerie}
-                    sx={{
-                        "&:hover": {
-                            color: '#868686',
-                        },
-                    }}/>
+                <div>
+                    {props.stars.map((value, i) => {
+                        if (value === 1) {
+                            return (<StarRoundedIcon className={classes.serie_secondRow_star} key={i}/>)
+                        } else {
+                            return (<StarOutlineRoundedIcon className={classes.serie_secondRow_star} key={i}/>)
+                        }
+                    })}
+                </div>
             </div>
+
+            <MoreHorizIcon
+                onClick={props.editSerie}
+                sx={{
+                    "&:hover": {
+                        color: '#868686',
+                    },
+                }}/>
         </article>
     );
 }
---
+++ series_book_frontend/src/test/API/epguidesAPI.js
@@ -0,0 +1,20 @@
+
+import fetch from "node-fetch"
+import axios from "axios"
+
+var url = " https://api.tvmaze.com";
+var searchUrl = "/search/shows?q="
+
+var searchValue = "Attack on Titan"
+
+axios.get(url + searchUrl + searchValue).then(function (response) {
+    let incomingData = response.data;
+    console.log(incomingData);
+
+    incomingData.map((data) => {
+        console.log(data.show.id)
+    })
+
+}).catch(function (error) {
+    console.log(error);
+});
