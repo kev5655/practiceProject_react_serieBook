517aaa78e6f83894255781c162f3b2a7be0e4176
kevin <kevin.zahn@bluewin.ch>
2022-09-18 20:49:21 +0200

    refactoring

--- serie_book_backend/src/main/java/kevProject/serie_book/api/AppUserController.java
+++ serie_book_backend/src/main/java/kevProject/serie_book/api/AppUserController.java
@@ -94,10 +94,10 @@ public class AppUserController {
 
             } catch (Exception exception) {
                 log.error("Error logging in: {}", exception.getMessage());
-                response.setHeader("error", exception.getMessage());
-                response.setStatus(FORBIDDEN.value());
                 Map<String, String> error = new HashMap<>();
                 error.put("error_message", exception.getMessage());
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(FORBIDDEN.value());
                 response.setContentType(APPLICATION_JSON_VALUE);
                 new ObjectMapper().writeValue(response.getOutputStream(), error);
             }
