7fbcf70b6917c36715f6a124908a291003c4eaba
kevin <kevin.zahn@bluewin.ch>
2022-09-23 14:03:20 +0200

    update props

--- frontend/src/components/app/manageSerie/ManageSerieForm.jsx
+++ frontend/src/components/app/manageSerie/ManageSerieForm.jsx
@@ -1,30 +1,45 @@
-import React, {useState} from 'react'
+import React, {forwardRef, useImperativeHandle, useState} from 'react'
 
 
 import InputText from "../../ui/form/InputText";
-import InputNumber from "../../ui/form/InputNumber";
 import InputStars from "../../ui/form/InputStars";
 import Btn from "../../ui/form/Btn";
 import InputDatepicker from "../../ui/form/InputDatepicker";
-
+import InputNumber from "../../ui/form/InputNumber";
 import {fetchData} from "../../utils/api"
 
 import classes from "./ManageSerieForm.module.css";
 
+
 // https://mui.com/material-ui/react-autocomplete/#autocomplete-autofill for LabelPicker
-const ManageSerieForm = (props) => {
+const ManageSerieForm = forwardRef ((props, ref) => {
 
     let defaultIsValid = false;
-    if (props.isEdit) {
+    if (props.isEdit === "true") {
         defaultIsValid = true;
     }
 
-    const [enteredTitle, setEnteredTitle] = useState({value: props.titleValue, isValid: defaultIsValid});
-    const [enteredSession, setEnteredSession] = useState({value: props.sessionValue, isValid: defaultIsValid});
-    const [enteredEpisode, setEnteredEpisode] = useState({value: props.episodeValue, isValid: defaultIsValid});
-    const [enteredStartDate, setEnteredStartDate] = useState({value: props.startDateValue, isValid: defaultIsValid});
-    const [enteredEndDate, setEnteredEndDate] = useState({value: props.endDateValue, isValid: defaultIsValid});
-    const [enteredStars, setEnteredStars] = useState({value: props.starsValue})
+    let serie = {}
+    if (props.editingSerie === undefined){
+        serie.id = ''
+        serie.title = ''
+        serie.session = ''
+        serie.episode = ''
+        serie.startDate = ''
+        serie.endDate = ''
+        serie.stars = ''
+        serie.createdDate = null
+    } else {
+        serie = props.editingSerie
+    }
+
+    const [enteredTitle, setEnteredTitle] = useState({value: serie.title, isValid: defaultIsValid});
+    const [enteredSession, setEnteredSession] = useState({value: serie.session, isValid: defaultIsValid});
+    const [enteredEpisode, setEnteredEpisode] = useState({value: serie.episode, isValid: defaultIsValid});
+    const [enteredStartDate, setEnteredStartDate] = useState({value: serie.startDate, isValid: defaultIsValid});
+    const [enteredEndDate, setEnteredEndDate] = useState({value: serie.endDate, isValid: defaultIsValid});
+    const [enteredStars, setEnteredStars] = useState({value: serie.stars})
+
     //TODO Validate Enterte E-Mail
     const titleHandler = (value) => {
         setEnteredTitle({value: value, isValid: true});
@@ -51,31 +66,41 @@ const ManageSerieForm = (props) => {
             enteredEpisode.isValid && enteredStartDate.isValid &&
             enteredEndDate.isValid) {
 
-            let id = props.id;
+            let manageSerie = getSerieDate();
 
-            const manageSerie = {
-                id: id,
-                title: enteredTitle.value,
-                session: enteredSession.value,
-                episode: enteredEpisode.value,
-                startDate: enteredStartDate.value,
-                endDate: enteredEndDate.value,
-                createdDate: props.createdDateValue ?? new Date(Date.now()),
-                stars: enteredStars.value,
-            }
             if (props.isEdit === true) {
                 await updateSerieIntoDB(manageSerie)
             } else {
                 delete manageSerie.id
-                id = await saveSerieIntoDB(manageSerie);
-                manageSerie.id = id;
+                manageSerie.id = await saveSerieIntoDB(manageSerie);
             }
             manageSerie.isEdit = props.isEdit;
             props.onManagedSerie(manageSerie);
         }
     }
 
+    const getSerieDate = () => {
+        return {
+            id: serie.id,
+            title: enteredTitle.value,
+            session: enteredSession.value,
+            episode: enteredEpisode.value,
+            startDate: enteredStartDate.value,
+            endDate: enteredEndDate.value,
+            createdDate: serie.createdDate ?? new Date(Date.now()),
+            stars: enteredStars.value,
+        };
+    }
 
+
+    useImperativeHandle(ref, () => ({
+        async deleteSerie(){
+            await fetchData("/api/serie/delete",
+                "Delete",
+                JSON.stringify(getSerieDate()),
+                'application/json')
+        }
+    }));
 
     let updateSerieIntoDB = async (updateSerie) => {
         await fetchData("/api/serie/update",
@@ -97,24 +122,24 @@ const ManageSerieForm = (props) => {
             <div className={`${classes.grid_title_item} ${classes.item}`}>
                 <InputText
                     placeholder='Serie Name'
-                    value={props.titleValue}
+                    value={enteredTitle.value}
                     onChange={titleHandler}/>
             </div>
 
             <div className={`${classes.grid_episode_season_item} ${classes.item}`}>
                 <InputNumber
                     placeholder='Staffel'
-                    value={props.sessionValue}
+                    value={enteredSession.value}
                     onChange={sessionHandler}/>
                 <InputNumber
                     placeholder='Folge'
-                    value={props.episodeValue}
+                    value={enteredEpisode.value}
                     onChange={episodeHandler}/>
             </div>
 
             <div className={`${classes.grid_stars_item} ${classes.item}`}>
                 <InputStars
-                    value={props.starsValue}
+                    value={enteredStars.value}
                     className={{
                         color: '#780000',
                         fontFamily: "\"JetBrains Mono\", sans-serif"
@@ -125,7 +150,7 @@ const ManageSerieForm = (props) => {
             <div className={`${classes.grid_date_item} ${classes.item} ${classes.space}`}>
                 <InputDatepicker
                     label='Start Date'
-                    value={props.startDateValue}
+                    value={enteredStartDate.value}
                     onChange={startDateHandler}
                 />
 
@@ -133,7 +158,7 @@ const ManageSerieForm = (props) => {
 
                 <InputDatepicker
                     label='End Date'
-                    value={props.endDateValue}
+                    value={enteredEndDate.value}
                     onChange={endDateHandler}
                 />
             </div>
@@ -159,6 +184,6 @@ const ManageSerieForm = (props) => {
 
         </form>
     )
-}
+})
 
 export default ManageSerieForm;
