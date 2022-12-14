b032e8577630f7c9a5a7740a53428c097c6afa34
Kevin Zahn <kevin.zahn@bluewin.ch>
2022-12-14 18:34:39 +0100

    update imports from SerieBookApplication

--- src/main/java/kevProject/serie_book/SerieBookApplication.java
+++ src/main/java/kevProject/serie_book/SerieBookApplication.java
@@ -1,21 +1,13 @@
 package kevProject.serie_book;
 
-import kevProject.serie_book.model.AppRole;
-import kevProject.serie_book.model.AppUser;
-import kevProject.serie_book.model.Serie;
-import kevProject.serie_book.service.AppUserService;
-import kevProject.serie_book.service.SerieService;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.util.ArrayList;
+
 
 @SpringBootApplication
 public class SerieBookApplication {
@@ -28,4 +20,6 @@ public class SerieBookApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+
 }
