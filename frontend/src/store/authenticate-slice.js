986dbc0be9ca1e9ac3b7cc6371e666282867462c
Kevin Zahn <kevin.zahn@bluewin.ch>
2022-12-16 16:59:40 +0100

    add singUpFailed-Reducer to authenticate-slice.js

--- frontend/src/store/authenticate-slice.js
+++ frontend/src/store/authenticate-slice.js
@@ -1,17 +1,24 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 
+const INIT_SingUpError = {
+    isFailed: false,
+    errorText: ''
+};
+// ToDo update loginFailed to login Error like SingUpError
 const authenticateSlice = createSlice({
     name: 'authentication',
     initialState: {
         isAuth: false,
         loginFailed: false,
+        singUpError: INIT_SingUpError,
         access_token: null,
         refreshToken: null,
     },
     reducers: {
         login(state, action){
             state.isAuth = true
+            state.singUpError = INIT_SingUpError
             state.access_token = action.payload.access_token
             state.refreshToken = action.payload.refresh_token
         },
@@ -22,6 +29,10 @@ const authenticateSlice = createSlice({
         },
         loginFailed(state){
             state.loginFailed = true
+        },
+        singUpFailed(state, action){
+            state.singUpError.isFailed = true
+            state.singUpError.errorText = action.payload.errorText
         }
 
     }
