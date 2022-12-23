20035c6870b42d9bbcde7128a41051daa74cc0ab
Kevin Zahn <kevin.zahn@bluewin.ch>
2022-12-23 14:28:24 +0100

    refactor InputDatepicker.jsx

--- frontend/src/components/ui/form/InputDatepicker.jsx
+++ frontend/src/components/ui/form/InputDatepicker.jsx
@@ -9,19 +9,19 @@ import './InputDatepicker.css'
 const InputDatepicker = (props) => {
 
     //ToDo vereinfachen
-    let date = props.value
-    let defaultDateValue = ""
-    let defaultDayValue = undefined;
-    let defaultMonthValue = undefined;
-    let defaultYearValue = undefined;
-    if (! (props.value === undefined || props.value === '' || props.value === null)) {
-        defaultDayValue = date.getDate();
-        defaultMonthValue = date.getMonth();
-        defaultYearValue = date.getFullYear();
-        defaultDateValue = new Date(defaultYearValue, defaultMonthValue,defaultDayValue);
-    }
+    let date = props.value === '' ? '' : new Date(props.value)
+    // let defaultDateValue = ""
+    // let defaultDayValue = undefined;
+    // let defaultMonthValue = undefined;
+    // let defaultYearValue = undefined;
+    // if (! (props.value === undefined || props.value === '' || props.value === null)) {
+    //     defaultDayValue = date.getDate();
+    //     defaultMonthValue = date.getMonth();
+    //     defaultYearValue = date.getFullYear();
+    //     defaultDateValue = new Date(defaultYearValue, defaultMonthValue,defaultDayValue);
+    // }
 
-    const [startDate, setStartDate] = useState(defaultDateValue);
+    const [startDate, setStartDate] = useState(date);
 
     const onChangeHandler = (date) => {
         setStartDate(date);
