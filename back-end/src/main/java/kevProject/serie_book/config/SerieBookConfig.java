c1514fcc8d9a5c43f6d7d443e19d710a108cf35d
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-01-19 16:27:21 +0100

    fix authentication bug

--- back-end/src/main/java/kevProject/serie_book/config/SerieBookConfig.java
+++ back-end/src/main/java/kevProject/serie_book/config/SerieBookConfig.java
@@ -12,7 +12,7 @@ public class SerieBookConfig {
                      @Value("${security.refresh_jwt_lifetime}") Long refreshJwtLifetime,
                      @Value("${security.jwt_foreword}") String jwtForeword,
                      @Value("${security.HMAC256_KEY}") String HMAC256Key){
-        Secrets secrets = new Secrets(accessJwtLifetime, refreshJwtLifetime, jwtForeword, HMAC256Key);
+        Secrets secrets = new Secrets(accessJwtLifetime, refreshJwtLifetime, HMAC256Key, jwtForeword);
         return secrets;
     }
 
